d2b2de9c13e5cc3698e63ac2322f3820935228e7
primax <primax2@bk.ru>
2020-06-01 17:13:32 +0300

    +List FIltering Test OK

---
+++ ListFiltering/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace ListFiltering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lst = GetIntegersFromList(new List<object>() { 1, 2, "a", "b", 0, 15 });
+            foreach(var l in lst)
+            {
+                Console.WriteLine(l);
+            }
+
+        }
+
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            return (List<int>)listOfItems.Where(x => x.GetType() == typeof(int)).Select(x => (int)x).ToList<int>();
+
+        }
+    }
+}
