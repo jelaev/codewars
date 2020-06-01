7d2eef4cee33ed3620f7ff121ec9a0870173456d
primax <primax2@bk.ru>
2020-06-01 16:29:00 +0300

    Tests OK

---
+++ DescendingOrder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace DescendingOrder
+{
+    class Program
+    {
+        public static int DescendingOrder(int num)
+        {
+            // Bust a move right here
+            var getStr = num.ToString();
+            List<string> massive = new List<string>();
+            foreach(var m in getStr)
+            {
+                massive.Add(m.ToString());
+            }
+            var sort = massive.OrderByDescending(m => m);
+            return Convert.ToInt32(string.Join("", sort));
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(DescendingOrder(17362));
+        }
+    }
+}
