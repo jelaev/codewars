c421e3fb446407b4b4e187d45dc1cfc24882f9de
primax <primax2@bk.ru>
2020-06-01 17:53:15 +0300

    +XO TEST OK

---
+++ ExesAndOhs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ExesAndOhs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(XO("ooxx"));
+            Console.WriteLine(XO("xooxx"));
+            Console.WriteLine(XO("ooxXm"));
+            Console.WriteLine(XO("zpzpzpp"));
+            Console.WriteLine(XO("zzoo"));
+        }
+
+        public static bool XO(string input)
+        {
+            var xv = input.Where(x => x.ToString().ToLower() == "x").Count();
+            var ov = input.Where(x => x.ToString().ToLower() == "o").Count();
+            if ((xv == ov) && (xv != 0 && ov != 0))
+                return true;
+            if (xv == 0 && ov == 0)
+                return true;
+            return false;
+        }
+    }
+}
