1baee6b985eb4339573126caaacce7c946da71e5
primax <primax2@bk.ru>
2020-06-01 17:38:21 +0300

    +TakeATenMinuteWalk TEST OK

---
+++ TakeATenMinuteWalk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TakeATenMinuteWalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsValidWalk(string[] walk)
+        {
+            if (walk.Length != 10)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < walk.Length - 1; i++)
+            {
+                if (walk[i] == walk[i+1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
