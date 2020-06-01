2513178d9a6cf22f62f8f0999f443e1d7414907b
primax <primax2@bk.ru>
2020-06-01 16:54:27 +0300

    +BitCounting Test OK

---
+++ BiCounting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BitCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountBits(1234));
+        }
+
+        public static int CountBits(int n)
+        {
+            return Convert.ToString(n, 2).Count<char>(x => x == '1');
+        }
+    }
+}
